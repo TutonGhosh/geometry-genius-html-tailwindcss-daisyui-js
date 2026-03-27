133ba2d957d79a51a0b6bd23575df321614e340a
TutonGhosh <404ghosh@gmail.com>
2026-03-27 23:13:56 +0600

    rectangle card completed

--- JS/script.js
+++ JS/script.js
@@ -8,13 +8,34 @@ function triangleClick()
   const height = parseFloat(triangleHeightInput.value);
 
   const result = 0.5 * base * height;
-  
+
   const AreaCalculator = document.getElementById("area-calculator");
   const p = document.createElement("p");
   p.classList.add("commonStyle");
   p.innerText = "Triangle = " + result + " cm²";
   AreaCalculator.appendChild(p);
 
-  triangleBaseInput.value = "";
-  triangleHeightInput.value = "";
+  triangleBaseInput.value = " ";
+  triangleHeightInput.value = " ";
+}
+
+// Retangle
+function rectangleClick()
+{
+  const rectangleWidthInput = document.getElementById('rectangle-width');
+  const rectangleLengthInput = document.getElementById('rectangle-length');
+
+  const width = rectangleWidthInput.value;
+  const length = rectangleLengthInput.value;
+
+  const result = width * length;
+
+  const AreaCalculator = document.getElementById('area-calculator');
+  const p = document.createElement('p');
+  p.classList.add("commonStyle");
+  p.innerText = "Rectangle = " + result + " cm²";
+  AreaCalculator.appendChild(p);
+
+  rectangleWidthInput.value = " ";
+  rectangleLengthInput.value = " ";
 }
