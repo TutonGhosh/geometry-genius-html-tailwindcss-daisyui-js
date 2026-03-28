8577f3ac91aa62e8d02df00d7d84575f758c3a28
TutonGhosh <404ghosh@gmail.com>
2026-03-28 23:56:32 +0600

    card completed

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,83 @@
 // Triangle
-function triangleClick() 
+function triangleClick()
 {
-  const triangleBaseInput = document.getElementById("triangle-base");
-  const triangleHeightInput = document.getElementById("triangle-height");
-
-  const base = parseFloat(triangleBaseInput.value);
-  const height = parseFloat(triangleHeightInput.value);
-
-  const result = 0.5 * base * height;
-
-  const AreaCalculator = document.getElementById("area-calculator");
-  const p = document.createElement("p");
-  p.classList.add("commonStyle");
-  p.innerText = "Triangle = " + result + " cm²";
-  AreaCalculator.appendChild(p);
-
-  triangleBaseInput.value = " ";
-  triangleHeightInput.value = " ";
+  const base = commonFunctionForGetValue('triangle-base');
+  const height = commonFunctionForGetValue('triangle-height');
+  let result = 0.5 * base * height; 
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Triangle', result);
+  // document.getElementById('triangle-base').value = " ";
+  // document.getElementById('triangle-height').value = " ";
 }
-
-// Retangle
+// Rectangle
 function rectangleClick()
 {
-  const rectangleWidthInput = document.getElementById('rectangle-width');
-  const rectangleLengthInput = document.getElementById('rectangle-length');
+  const width = commonFunctionForGetValue('rectangle-width');
+  const length = commonFunctionForGetValue('rectangle-length');
+  let result = width * length;
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Rectangle', result);
+  // document.getElementById('rectangle-width').value = " ";
+  // document.getElementById('rectangle-length').value = " ";
+}
+// Parallelogram
+function parallelogramClick()
+{
+  const base = commonFunctionForGetValue('parallelogram-base');
+  const height = commonFunctionForGetValue('parallelogram-height');
+  let result = base * height;
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Parallelogram', result);
+  // document.getElementById('parallelogram-base').value = " ";
+  // document.getElementById('parallelogram-height').value = " ";
+}
+// Rhombus
+function rhombusClick()
+{
+  const diagonal1 = commonFunctionForGetValue('rhombus-diagonal-1');
+  const diagonal2 = commonFunctionForGetValue('rhombus-diagonal-2');
+  let result = 0.5 * diagonal1 * diagonal2;
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Rhombus', result);
+  // document.getElementById('parallelogram-base').value = " ";
+  // document.getElementById('parallelogram-height').value = " ";
+}
+// Pentagon
+function pentagonClick()
+{
+  const perimeter = commonFunctionForGetValue('pentagon-perimeter');
+  const apothem = commonFunctionForGetValue('pentagon-apothem');
+  let result = 0.5 * perimeter * apothem;
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Penatgon', result);
+  // document.getElementById('parallelogram-base').value = " ";
+  // document.getElementById('parallelogram-height').value = " ";
+}
+// Ellipse
+function ellipseClick()
+{
+  const aAxis = commonFunctionForGetValue('ellipse-a-axis');
+  const bAxis = commonFunctionForGetValue('ellipse-b-axis');
+  let result = 3.14159 * aAxis * bAxis;
+  result = parseFloat(result.toFixed(2));
+  appendFunction('area-calculator', 'Ellipse', result);
+  // document.getElementById('parallelogram-base').value = " ";
+  // document.getElementById('parallelogram-height').value = " ";
+}
 
-  const width = rectangleWidthInput.value;
-  const length = rectangleLengthInput.value;
 
-  const result = width * length;
-
-  const AreaCalculator = document.getElementById('area-calculator');
-  const p = document.createElement('p');
+// Repetitive Task Function
+function commonFunctionForGetValue(inputID)
+{
+  const inputText = document.getElementById(inputID);
+  const inputValue = parseFloat(inputText.value);
+  return inputValue;
+}
+function appendFunction(areaID, name, result)
+{
+  const AreaCalculator = document.getElementById(areaID);
+  const p = document.createElement("p");
   p.classList.add("commonStyle");
-  p.innerText = "Rectangle = " + result + " cm²";
+  p.innerText = name + " = " + result + " cm²";
   AreaCalculator.appendChild(p);
-
-  rectangleWidthInput.value = " ";
-  rectangleLengthInput.value = " ";
 }
